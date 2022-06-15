da77535d8cc7c08df53f05c64ab51a82ca7ffbb7
Cassanova Yuna Revayanto <cassanovarevayanto@gmail.com>
2022-06-15 21:45:55 +0700

    xsaatu link

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#67E8F9',
+        dark: '#0F172A',
+      },
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+}
